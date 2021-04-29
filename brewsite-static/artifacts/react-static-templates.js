6052ec1aa4e349490d44bf54fd1966b02d265ab4
Paul Lohmuller <plohm12@gmail.com>
2021-04-29 09:31:14 -0500

    Init react-static redesign

---
+++ brewsite-static/artifacts/react-static-templates.js
@@ -0,0 +1,10 @@
+
+
+
+// Template Map
+export default {
+  '__react_static_root__/node_modules/react-static/lib/browser/components/Default404': require('__react_static_root__/node_modules/react-static/lib/browser/components/Default404').default
+}
+
+export const notFoundTemplate = '__react_static_root__/node_modules/react-static/lib/browser/components/Default404'
+
---
+++ brewsite-static/src/index.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { AppContainer } from 'react-hot-loader'
+
+// Your top level component
+import App from './App'
+
+// Export your top level component as JSX (for static rendering)
+export default App
+
+// Render your app
+if (typeof document !== 'undefined') {
+  const target = document.getElementById('root')
+
+  const renderMethod = target.hasChildNodes()
+    ? ReactDOM.hydrate
+    : ReactDOM.render
+
+  const render = Comp => {
+    renderMethod(
+      <AppContainer>
+        <React.Suspense fallback={<div>Loading...</div>}>
+        <Comp />
+        </React.Suspense>
+      </AppContainer>,
+      target
+    )
+  }
+
+  // Render!
+  render(App)
+
+  // Hot Module Replacement
+  if (module && module.hot) {
+    module.hot.accept('./App', () => {
+      render(App)
+    })
+  }
+}
