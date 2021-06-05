19a523b96512cd493806d9ce3440fc1586bda76b
Paul Lohmuller <plohm12@gmail.com>
2021-06-05 13:08:34 -0500

    Add goatcounter script for web analytics

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,12 +1,22 @@
 import React from 'react';
 
-export const onRenderBody = ({ setHeadComponents, setHtmlAttributes }) => {
+export const onRenderBody = ({
+  pathname,
+  setHeadComponents,
+  setHtmlAttributes,
+  setPostBodyComponents,
+}) => {
   setHtmlAttributes({
     lang: 'en-US'
   });
   setHeadComponents([
     <link key="pure-css" rel="stylesheet" href="https://unpkg.com/purecss@2.0.6/build/pure-min.css" />,
     <link key="pure-css-responsive-grid" rel="stylesheet" href="https://unpkg.com/purecss@2.0.5/build/grids-responsive-min.css" />,
-    // <link key="local-styles" rel="stylesheet" href="/styles.css" />
+    <script key="goat-counter-script" data-goatcounter="https://plohm12.goatcounter.com/count" async src="/goat-counter.js"></script>
+  ]);
+  setPostBodyComponents([
+    <noscript key="goat-counter-noscript">
+      <img src={`https://plohm12.goatcounter.com/count?q=noscript&p=${encodeURIComponent(pathname)}`} />
+    </noscript>
   ]);
 };
